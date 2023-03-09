1a6513c64d77a2959c2cf053a2c45e7bb5b130aa
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-03-09 11:54:24 +0300

    :art: implementing jwt service

---
+++ src/main/java/com/springSecurity/GettingStartedWithSpringSecurity/config/JwtAuthenticatorFilter.java
@@ -0,0 +1,41 @@
+package com.springSecurity.GettingStartedWithSpringSecurity.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticatorFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail=jwtService.extractUserName(jwt);
+
+    }
+
+}
