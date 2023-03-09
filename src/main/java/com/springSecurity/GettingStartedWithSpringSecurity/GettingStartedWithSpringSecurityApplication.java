50379942bbd4251ae04901e1cc18c07c9bb22b3b
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-03-09 08:46:01 +0300

    :tada: initial commit

---
+++ src/main/java/com/springSecurity/GettingStartedWithSpringSecurity/GettingStartedWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurity.GettingStartedWithSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springSecurity/GettingStartedWithSpringSecurity/GettingStartedWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurity.GettingStartedWithSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
